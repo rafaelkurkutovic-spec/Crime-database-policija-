b2fcb668ccddcaa42cfe5615a3952efdb0f4744a
rafaelkurkutovic-spec <rafaelkurkutovic@gmail.com>
2026-05-13 00:52:42 +0200

    Add files via upload

---
+++ Crime_Database(Rafael_kurkutovic)/Crime_Database(Rafael_kurkutovic)/records.h
@@ -0,0 +1,27 @@
+#ifndef RECORDS_H
+#define RECORDS_H
+
+typedef struct {
+    int id;
+    char firstName[50];
+    char lastName[50];
+    int age;
+    char crime[100];
+    char description[250];
+    char status[20]; // OPEN / CLOSED
+
+} Record;
+
+extern Record* records;
+extern int count;
+extern int nextID;
+
+void loadRecords();
+void saveRecords();
+void createRecord();
+void editRecord();
+void deleteRecord();
+void displayShort();
+void displayFull();
+
+#endif
