7ae6f0d62f6b272b4e56745b3f742ff2be80a8ff
rafaelkurkutovic-spec <rafaelkurkutovic@gmail.com>
2026-05-27 00:19:42 +0200

    Add files via upload

---
+++ Crime_Database(Rafael_kurkutovic) – kopija/Crime_Database(Rafael_kurkutovic)/records.h
@@ -0,0 +1,43 @@
+#ifndef RECORDS_H
+#define RECORDS_H
+
+#define MAX_NAME 50
+#define MAX_CRIME 100
+#define MAX_DESCRIPTION 250
+
+typedef enum {
+    OPEN,
+    CLOSED,
+    COLD
+} Status;
+
+typedef struct {
+    int id;
+    char firstName[MAX_NAME];
+    char lastName[MAX_NAME];
+    int age;
+    char crime[MAX_CRIME];
+    char description[MAX_DESCRIPTION];
+    Status status;
+} Record;
+
+extern Record* records;
+extern int count;
+extern int nextID;
+
+void loadRecords();
+void saveRecords();
+
+void createRecord();
+void deleteRecord();
+void updateRecord();
+
+void displayShort();
+void displayFull();
+
+void searchByID();
+void searchByName();
+
+void sortRecords();
+
+#endif
